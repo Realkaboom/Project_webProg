aa8b24f17a38260d99d7a8d67f5bf795b8fa4c6b
Realkaboom <145574090+Realkaboom@users.noreply.github.com>
2026-01-18 11:00:42 +0700

    tes

--- api/index.php
+++ api/index.php
@@ -1,3 +1,7 @@
 <?php
-error_reporting(E_ALL & ~E_DEPRECATED);
+
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT);
+ini_set('display_errors', '0');
+ini_set('display_startup_errors', '0');
+
 require __DIR__ . '/../public/index.php';
--- public/index.php
+++ public/index.php
@@ -1,26 +1,8 @@
 <?php
 
-use Illuminate\Foundation\Application;
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT);
+
+use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-require __DIR__.'/../vendor/autoload.php';
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$app->handleRequest(Request::capture());
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
